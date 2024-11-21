12eae15c6c87ca66e4661d48cd00bb7a59b69e0a
MALYADRI BATHULA <127408429+BATHULAMALYADRI@users.noreply.github.com>
2024-11-21 18:57:11 +0530

    this is a commit message

---
+++ src/com/lbrce/calculator/Calculator2.java
@@ -0,0 +1,31 @@
+package com.lbrce.calculator;
+
+public class Calculator2 {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator2 calc = new Calculator2();
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(5, 3));
+        System.out.println("Division: " + calc.divide(6, 2));
+    }
+}
+
